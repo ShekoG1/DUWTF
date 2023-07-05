c294885ebfd90f8b1757edd554dab7226f68309a
Shekhar Maharaj <shekharmaharaj2905@gmail.com>
2023-07-05 19:50:31 +0200

    Added comment base code

--- Pages/ViewBlog/index.php
+++ Pages/ViewBlog/index.php
@@ -1,5 +1,13 @@
 <?php
     $postId = explode("-",$_POST['id'])[1];
+    // $colorHex = $_POST['colorHex'];
+    // $user = $_COOKIE['DUWTF_USER'];
+    // $userId = $user->id;
+
+    // TEST
+    $userId = 1;
+    // END TEST
+
     
     // Get Post
     $postCurl = curl_init();
@@ -91,9 +99,8 @@ $postContent = $postResponsedata[0]->post_content;
     <link rel="stylesheet" href="./../../res/bootstrap/css/bootstrap.css">
     <script src="./../../res/bootstrap/js/bootstrap.js"></script>
     <!-- STYLE -->
-    <link rel="stylesheet" href="./../../style/global.css">
-    <link rel="stylesheet" href="./../../style/landing.css">
-    <link rel="stylesheet" href="./../../style/home.css">
+    <link rel="stylesheet" href="./../../style/globals.css">
+    <link rel="stylesheet" href="./../../style/view.css">
 </head>
 <body>
     <?php
@@ -125,6 +132,14 @@ $postContent = $postResponsedata[0]->post_content;
             </div>
             <div id="comments">
                 <h2>Comments (<?php echo $postComments; ?>)</h2>
+                <div id="make-comment">
+                    <input type="text" placeholder="Leave your comment here..." id="comment">
+                    <button onclick="makeComment()">
+                        <i>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16"> <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"/> </svg>
+                        </i>
+                    </button>
+                </div>
                 <?php
                     if($postComments > 0){
                         foreach ($postCommentdata->data as $comment) {
@@ -152,5 +167,25 @@ $postContent = $postResponsedata[0]->post_content;
             </div>
         </div>
     </div>
+    <script>
+        function makeComment(){
+            let comment = 
+            var formdata = new FormData();
+            formdata.append("userId", <?php echo $userId; ?>);
+            formdata.append("postId", <?php echo $postId; ?>);
+            formdata.append("comment", comment);
+
+            var requestOptions = {
+            method: 'POST',
+            body: formdata,
+            redirect: 'follow'
+            };
+
+            fetch("http://localhost/projects/DearUniverseWTF/api/endpoints/posts/makeComment.php", requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+        }
+    </script>
 </body>
 </html>
